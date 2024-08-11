b12efe06c04d1db252cd4f50829c54e181e0a58b
Abhigitat <vkchandekar12@gmail.com>
2024-08-11 20:16:35 +0530

    Adding Input

--- src/inputs/KeyboardInputs.java
+++ src/inputs/KeyboardInputs.java
@@ -3,8 +3,16 @@ package inputs;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class KeyboardInputs implements KeyListener{
+import main.GamePanel;
 
+public class KeyboardInputs implements KeyListener{
+	
+	private GamePanel gamePanel;
+	
+	public KeyboardInputs(GamePanel gamePanel) {
+		this.gamePanel=gamePanel;
+	}
+	
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
@@ -19,8 +27,24 @@ public class KeyboardInputs implements KeyListener{
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_W:
+			gamePanel.changeYDelta(-5);
+			System.out.println("W");
+			break;
+		case KeyEvent.VK_S:
+			gamePanel.changeYDelta(5);
+			System.out.println("S");
+			break;
+		case KeyEvent.VK_A:
+			gamePanel.changeXDelta(-5);
+			System.out.println("A");
+			break;
+		case KeyEvent.VK_D:
+			gamePanel.changeXDelta(5);
+			System.out.println("D");
+			break;
+		}
 	}
 	
 
