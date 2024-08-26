bd9327372f91e21add75def6982a1ee58a204c8b
Abhigitat <vkchandekar12@gmail.com>
2024-08-26 22:40:43 +0530

    Smoothing Game Animation

--- src/main/Game.java
+++ src/main/Game.java
@@ -6,6 +6,8 @@ public class Game implements Runnable{
 	private GamePanel  gamePanel;
 	private Thread gameThread;
 	private final int FPS_SET=120;
+	private final int UPS_SET=200;
+	
 	public Game(){
 		gamePanel=new GamePanel();
 		gameWindow=new GameWindow(gamePanel);
@@ -18,25 +20,48 @@ public class Game implements Runnable{
 		gameThread.start();
 	}
 	
+	public void update() {
+		gamePanel.updateGame();
+	}
+	 
 	@Override
 	public void run() {
 		double timePerFrame = 1000000000.0/FPS_SET;
-		long lastFrame=System.nanoTime();
-		long now=System.nanoTime();
+		double timePerUpdate = 1000000000.0/UPS_SET;
+		
+		long previousTime=System.nanoTime();
+		
 		int frames=0;
+		int update=0;
 		long lastChecked=System.currentTimeMillis();
+		
+		double deltaU = 0;
+		double deltaF = 0;
+		
 		while(true) {
-			now=System.nanoTime();
+			long currentTime=System.nanoTime();
 			
-			if(now - lastFrame>=timePerFrame) { 
-				gamePanel.repaint();
-				lastFrame=now; 
-				frames++;   
+			deltaU += (currentTime - previousTime)/timePerUpdate;
+			deltaF += (currentTime - previousTime)/timePerFrame;
+			
+			previousTime=currentTime;
+			if(deltaU>=1.0) {
+				update();
+				update++;
+			   	deltaU--;
 			}
+			
+			if(deltaF>=1.0) {
+				gamePanel.repaint();
+				frames++; 
+				deltaF--;
+			}
+			
 			if(System.currentTimeMillis()-lastChecked>=1000) {
 				lastChecked=System.currentTimeMillis();
-				System.out.println("FPS:"+frames);
-				frames=0; 
+				System.out.println("FPS:"+frames+ "| UPS:"+update);
+				frames=0;
+				update=0;
 			}
 		}
 	}
