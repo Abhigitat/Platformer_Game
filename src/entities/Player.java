90b1ae064e2e0261b36d6d823820e518cc6f437a
Abhigitat <vkchandekar12@gmail.com>
2024-11-01 22:25:29 +0530

    Changes

---
+++ src/entities/Player.java
@@ -0,0 +1,18 @@
+package entities;
+
+public class Player extends Entity{
+	
+	public Player(float x, float y) {
+		super(x, y);
+	}
+	
+	public void update(){
+		
+	}
+	
+	public void render(){
+		
+	}
+	 
+	
+}
--- src/main/GameWindow.java
+++ src/main/GameWindow.java
@@ -7,12 +7,12 @@ public class GameWindow {
 	public GameWindow(GamePanel gamePanel) {
 		
 		jframe= new JFrame();
-		
-		
 		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
 		jframe.add(gamePanel);
 		jframe.setLocationRelativeTo(null);
-		jframe.setResizable(false);
+		jframe.setResizable(true);
 		jframe.pack();
 		jframe.setVisible(true);
 	}
