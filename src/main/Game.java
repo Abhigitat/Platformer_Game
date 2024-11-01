2742600eee0d65b542747cb2242af15993c7bd24
Abhigitat <vkchandekar12@gmail.com>
2024-11-02 00:24:50 +0530

    Adding More Animation

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,5 +1,9 @@
 package main;
 
+import java.awt.Graphics;
+
+import entities.Player;
+
 public class Game implements Runnable{
 	
 	private GameWindow gameWindow;
@@ -7,13 +11,19 @@ public class Game implements Runnable{
 	private Thread gameThread;
 	private final int FPS_SET=120;
 	private final int UPS_SET=200;
+
+	private Player player;
 	
 	public Game(){
-		gamePanel=new GamePanel();
+		initClasses();
+		gamePanel=new GamePanel(this);
 		gameWindow=new GameWindow(gamePanel);
 		gamePanel.requestFocus();
 		startGameLoop();
 		
+	}
+	private void initClasses() {
+		player=new Player(200, 300);
 	}
 	private void startGameLoop() {
 		gameThread = new Thread(this);
@@ -21,7 +31,11 @@ public class Game implements Runnable{
 	}
 	
 	public void update() {
-		gamePanel.updateGame();
+		player.update();
+	}
+
+	public void render(Graphics g){
+		 player.render(g);
 	}
 	 
 	@Override
@@ -64,5 +78,12 @@ public class Game implements Runnable{
 				update=0;
 			}
 		}
+	}
+	public void windowFocusLost() {
+		player.resetDirBooleans();
+	}
+
+	public Player getPlayer(){
+		return player;
 	}
 }
